9852ac60109352098b00ecd6f50ef59b1ab191bf
Enzo Mallard <enzo@mallard.ml>
2018-10-19 18:57:43 +0200

    Plop

--- js/open.js
+++ js/open.js
@@ -5,8 +5,9 @@ function openfolder(){
 
     var fold = $(this).attr('class').split(/\s+/)[1];
 
+    if($(`.f_${fold}`).length)return;
 
-    $('.opened').append(`<div class="window f_${fold} last"><div class="top"></div><div class="content"></div></div>`)
+    $('.opened').append(`<div class="window f_${fold} last"><div class="top"><span>X</span></div><div class="content"></div></div>`)
     for(x of folders[fold]){
         ext = ['png', 'jpeg', 'jpg', 'bmp'][Math.floor(Math.random()*4)]
         $('.opened .window.last .content').append(`<div style="left: 0px; top: 0px;background-image: url('/res/${x}.png');"  draggable="true" class="file contained ${x}">${x}.png</div>`);
@@ -16,10 +17,22 @@ function openfolder(){
         containment: ".content"
     })
 
+    $('.opened .window.last .top span').click(function(){
+        $(this).parent().parent().css("opacity", 1).animate({opacity: 0},null,null,function(){$(this).remove()});
+    })
+
     $('.opened .window').draggable({
         containment: '#screen'
     }).resizable()
 
+    $(".opened .last .file, .folder").click(function (e) {
+        console.log("show")
+        selectElementContents(this)
+        e.cancelBubble = true; // Keep for compat
+        e.stopPropagation();
+    })
+
+    $(".opened .last").removeClass("last")
 }
 
 $(() => {
